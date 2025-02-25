361f91942d7ac10e4963b0163479866091922db9
lilchizh <xlilchizhx@gmail.com>
2025-02-25 13:15:03 +0300

    integral 1.2.1

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,9 +1,9 @@
 import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
-    [ChainId.Base]: '0xC3c535ffD4E72d3EB6a47db615b9Db43ec82B141',
+    [ChainId.Base]: '0xf03875b5Ec5eAc83cab83A6c2ab17844304AA7a0',
 }
 
 export const POOL_INIT_CODE_HASH = {
-    [ChainId.Base]: '0xb3fc09be5eb433d99b1ec89fd8435aaf5ffea75c1879e19028aa2414a14b3c85',
+    [ChainId.Base]: '0x0c1cd6d44fb0b21bff5173934487e28385d043c148fe7a24b4887edb35d09785',
 }
