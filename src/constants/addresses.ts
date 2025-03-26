0e93e0fd9c39f72d995423c0620848d2c60b4306
lilchizh <xlilchizhx@gmail.com>
2025-03-26 16:05:27 +0300

    add base sepolia

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -2,8 +2,10 @@ import { ChainId } from "./chainIds"
 
 export const POOL_DEPLOYER_ADDRESSES = {
     [ChainId.Base]: '0x02e6f07f6E908245C9f1d83d92b84d0a4815691c',
+    [ChainId.BaseSepolia]: '0x32e3F485696b2C6dBBc5C83A5Cb803Af72e1ecF3'
 }
 
 export const POOL_INIT_CODE_HASH = {
     [ChainId.Base]: '0xa18736c3ee97fe3c96c9428c0cc2a9116facec18e84f95f9da30543f8238a782',
+    [ChainId.BaseSepolia]: '0xa18736c3ee97fe3c96c9428c0cc2a9116facec18e84f95f9da30543f8238a782'
 }
